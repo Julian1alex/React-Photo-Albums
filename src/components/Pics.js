877ab54fa753d12c3a27c0bbd6de8499cff1e3c8
Alexander Gonzalez <julian1alex@gmail.com>
2019-11-04 09:05:39 -0800

    3rd page solved

--- src/components/Pics.js
+++ src/components/Pics.js
@@ -7,24 +7,23 @@ function Pic (props) {
     const id = props.match.params.id 
 
     useEffect (() => {
-axios.get(`/Pics/${id}?_embed=pictures`).then(resp => {
-    setPictures(resp.data.pictures)
+axios.get(`/Albums/${id}`).then(resp => {
+    setPictures(resp.data)
 })
-}, [])
+}, [id])
 
 return (
     <div className="background">
     <div className="gcontainer">
-    {pictures.map((e)=>(
-    <Link to= {"/Albums/" + e.id} key={"key" + e.id}>
+    <Link to= {"/Albums/" + pictures.id} key={"key" + pictures.id}>
     <div class="gallery-image2">
              <div class="img-box2">
-             <img className="albumsize" src = {e.url}/>
+             <img className="albumsize" src = {pictures.url}/>
              </div>
              </div>    
     </Link>
     
-        ))}
+        
     </div>
     </div>
 )
